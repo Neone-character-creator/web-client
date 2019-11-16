015050da6574f5d5dc030768f1e9c9a0b8b0b1e5
Damie <damienmarble@gmail.com>
2019-11-16 14:57:37 -0500

    Open downloaded pdf in new tab.

--- src/DownloadModal.js
+++ src/DownloadModal.js
@@ -2,8 +2,20 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import spinner from "./ajax-loader.gif";
 
 export default class DownloadModal extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {};
+
+        this.downloadStart = () => {
+            this.setState.bind(this, {
+                downloadInProgress: true
+            });
+            window.open(this.props.exportUrl, "_target");
+        }
+    }
 
     render() {
         return (<Modal show={this.props.show}>
@@ -14,10 +26,13 @@ export default class DownloadModal extends React.Component {
                 <Modal.Body>
                     <Container>
                         <Row>
-                            <a href={this.props.exportUrl}>
+                            <button className="btn btn-link" onClick={this.downloadStart} >
                                 Click here to download
-                            </a>
+                            </button>
                         </Row>
+                        {this.state.downloadInProgress && <Row>
+                            <img src={spinner}/>
+                        </Row>}
                     </Container>
 
                 </Modal.Body>
